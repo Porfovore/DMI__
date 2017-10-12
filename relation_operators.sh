d87a400c93777ba1974802b2a8a0ec888ae28106
roberts.mukans@gmail.com <avidonso@gmail.com>
2017-10-12 09:55:01 +0300

    20170928_09_50

---
+++ relation_operators.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#piemers1
+#a=$1
+#b=$2
+
+#if [ $a -eq $b ]
+#then 
+    #echo "$a -eq $b : a is equal to b"
+#else 
+    #echo "$a -eq $b: a is not equal to b"
+#fi
+
+#piemers2
+#a=$1
+#b=$2
+
+#if [ $a -ne $b ]
+#then 
+    #echo "$a -ne $b : a is not  equal to b"
+#else 
+    #echo "$a -ne $b : a is  equal to b"
+#fi
+
+
---
+++ shell_operators.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#piemers1
+#val=`expr 2 + 2 `
+#echo "Total value : $val"
+
+#piemers2
+#a=$1  pervoje chislo
+#b=$2  vtoroje chislo
+#val1=`expr $a + $b`
+#echo "Total value : "$val1
+
+#piemers3
+#a=$1
+#b=$2
+#val2=`expr $a - $b`
+#echo "Total value : "$val2
+
+#piemers4
+#a=$1
+#b=$2
+#val3=`expr $a \* $b` ymnozhenie
+#echo "Total value : "$val3
+
+
+#piemers5 
+#a=$1
+#b=$2
+#val4=`expr $b / $a`  delenie
+#echo "Total value : "$val4
+
+#piemers6
+#a=$1
+#b=$2
+#val5=`expr $b % $a`  
+#echo "Total value : "$val5
+
+
+
+#piemers7
+#a=$1
+#b=$2
+#val6=`a = $b`
+#echo "Total value : "$val6
+
+#piemers8
+#if [ $# == 2 ] #ljubaja peremennaja ,posle znakov ravno ykazivaetsja kolichestvo peremennix
+#then  
+#a=$1
+#b=$2
+#val7=`expr $a + $b`
+#echo "Total value : "$val7
+#fi
+
