c2dfb280f54c21e36432aa26f6a4d77cce9947f6
roberts.mukans@gmail.com <avidonso@gmail.com>
2017-11-02 11:35:31 +0200

    20171102

---
+++ PYTHON/hello.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+
+# https://www.tutorialspoint.com/unix_commands/echo.htm
+# Example 3. -> 6.
+# echo -e '...' -> print "..."
+
+print "Hello World!"
+
+
+
+# Example 3
+
+print  "Here \bthe \bspaces \bare \bbackspaced."
+'''
+
+# Example 4
+print "Here \nthe \nspaces \nare \nnewlined."
+'''
+
+# Example 5 
+
+print "Here \tthe \tspaces \thave \thorizontal \ttab \tspaces."
+
+'''
+
+Here	|He	|
+H	|Hehe	|
+1234567812345678
+'''
+# Example 6
+
+print "Here \vthe \vspaces \vhave \vvertical \vtab \vspaces. \n\t\t\t\t\tspaces. \n\t\t\t\tspaces. \n\t\t\tspaces. \n\t\tspaces. \n\tspaces. \nspaces."
+
+x=input("ievadiet x\n")
+y=input("ievadiet y\n")
+print "x = ", x, "y = ", y, "summa = ", x + y 
+
