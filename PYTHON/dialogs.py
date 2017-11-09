6c928d514d8d5ba18ea2ce9704b38928575b045e
roberts.mukans@gmail.com <avidonso@gmail.com>
2017-11-09 09:52:43 +0200

    20171102

---
+++ PYTHON/dialogs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+a = input("Lietotāj , lūdzu, ievadi kaut ko: ")
+print "Tu ievadīji lielumu ar %s dat tipu"%(type(a))
+print a * a
+print a + a
+
+a = raw_input("Lietotāj , lūdzu, ievadi kaut ko: ")
+print "Tu ievadīji lielumu ar %s dat tipu"%(type(a))
+
+#print a * a
+print a + a
+'''
+
+a = raw_input("Lietotāj, lūdzu, ievadi savu vārdu: ")
+b = raw_input("Lietotāj, lūdzu, ievadi savu uzvārdu: ")
+print "Tātad, Tevi sauc - %s"%(a + ' ' +b)
