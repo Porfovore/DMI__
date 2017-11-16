16d752cd241de1c1d60301c901cb083e4e307c1f
roberts.mukans@gmail.com <avidonso@gmail.com>
2017-11-16 11:49:17 +0200

    20171102

---
+++ PYTHON/kodi.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+'''
+a = input("Lietotāj, lūdzu, ievadi skaitli: ")
+
+if a > 0:
+    print "Tu esi ievadījis pozitīvu skaitli"
+elif a == 0:
+    print "Tu esi ievadījis NULLI"
+else:
+    print "Tu esi ievadījis negatīvu skaitli"
+print "Bet šo tekstu Tu redzēsi katru reizi!!!"
+'''
+
+'''
+if a > 0:
+    print "Tu esi ievadījis pozitīvu skaitli"
+else:
+    print "Tu neesi ievadījis pozitīvu skaitli"
+
+'''
+a = input("Lietotāj, lūdzu, ievadi beigu vērtību: ")
+
+k = 1
+while k <= a:
+    print "Mainīgā k kārtējā vērtība ir: %d\n"%(k),
+    k = k + 1
+print "Beigas!"
+																
